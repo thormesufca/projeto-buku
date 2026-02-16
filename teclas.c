4f9c8aa9ec26d13724fccd8a92a05b29885865eb
Thormes Filgueira Leite Pereira <thormes@gmail.com>
2026-02-16 15:26:24 -0300

    Adição de leitura de caracteres para melhorar movimentação durante semeadura

---
+++ teclas.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <conio.h>
+
+int ler_caracter()
+{
+    char c;
+    while (1)
+    {
+        c = getch();
+        if (c == 27 || c == 3)
+        {
+            printf("Programa Encerrado!\n");
+            exit(1);
+        }
+        if (c == 0 || c == -32)
+        {
+            c = getch();
+            switch (c)
+            {
+            case 72:
+                printf("UP\n");
+                return 0;
+            case 75:
+                printf("LEFT\n");
+                return 3;
+            case 80:
+                printf("DOWN\n");
+                return 2;
+            case 77:
+                printf("RIGHT\n");
+                return 1;
+            default:
+                printf("Tecla invalida, use teclas direcionais ou WASD. Z para desfazer ultimo movimento\n");
+                break;
+            }
+        }
+        else if (c == 26)
+        { // Ctrl + Z
+            return 99;
+        }
+        else
+        {
+            switch (c)
+            {
+            case 'W':
+            case 'w':
+                return 0;
+            case 'D':
+            case 'd':
+                return 1;
+            case 'S':
+            case 's':
+                return 2;
+            case 'A':
+            case 'a':
+                return 3;
+            case 'Z':
+            case 'z':
+                return 99;
+            default:
+                break;
+            }
+        }
+    }
+    return 0;
+}
---
+++ teclas.h
@@ -0,0 +1,6 @@
+/**
+ * @brief Realiza leitura de teclas do teclado e movimenta as peças caso seja digitada uma tecla válida
+ *
+ * @return int 0 -> Cima; 1 -> Direita; 2 -> Baixo; 3 -> Esquerda; 99 -> Desfazer
+ */
+int ler_caracter();
